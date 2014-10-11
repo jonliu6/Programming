6e26e09b0afb2deae8661aef8fe88af4e694c550
Jon <jonliu6@gmail.com>
2014-10-10 23:38:53 -0700

    RESTful Web Service implemented by RestEasy

---
+++ JAXRSDemo2/src/main/java/org/freecode/demo/rest/Employee.java
@@ -0,0 +1,23 @@
+package org.freecode.demo;
+
+
+import java.util.Date;
+
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.NamedQueries;
+//import javax.persistence.NamedQuery;
+//import javax.persistence.Table;
+
+//@XmlRootElement
+//@XmlAccessorType(XmlAccessType.FIELDS)
+public class Employee {
+	
+	public String employeeID;
+    
+	public Integer serviceInYears;
+    
+	public Date startDate;
+  
+}
