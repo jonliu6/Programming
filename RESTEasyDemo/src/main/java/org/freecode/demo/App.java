e68a6ca6da831b2b6c3e31549a42193a1f501b75
unknown <jonliu6@gmail.com>
2015-11-16 21:56:18 -0800

    initial

---
+++ RESTEasyDemo/src/main/java/org/freecode/demo/App.java
@@ -0,0 +1,28 @@
+package org.freecode.demo;
+
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try {
+            ClientRequest request = new ClientRequest(
+                    "http://localhost:8080/RESTEasyDemo/rest/demo/book/add");
+                request.accept("application/json");
+
+                String input = "{\"isbn\":\"99999\",\"title\":\"Rest Book\",\"publishedOn\":1447487315336,\"pageNo\":999}";
+                request.body("application/json", input);
+                    
+                ClientResponse<String> response = request.post(String.class);
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
