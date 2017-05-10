7522dceae3f9a73d010955eca952a2cd5728f0d6
Liu <jonliu@bchydro.adroot.bchydro.bc.ca>
2017-05-09 23:21:15 -0700

    added Python example

---
+++ PythonDemo/lambdaDemo.py
@@ -0,0 +1,7 @@
+dbl = lambda x:x * 2
+
+calc = lambda x,y:(x+y,x-y,x*y,x/y)
+
+
+print(dbl(4))
+print(calc(13,8))
---
+++ PythonDemo/xmlFormatter.py
@@ -0,0 +1,20 @@
+import sys
+from xml.etree import ElementTree
+from xml.dom import minidom
+
+argCnt = len(sys.argv)
+if argCnt < 2:
+    print "Usage: " + sys.argv[0] + " <source XML file>" 
+    sys.exit(1)
+	
+xmlFName = sys.argv[1]
+dom = minidom.parse(xmlFName)
+xmlContent = dom.toprettyxml()
+
+print xmlContent
+
+xmlNewFName = xmlFName + ".new"
+f = open(xmlNewFName, 'w')
+f.write(xmlContent)
+f.close()
+print xmlFName + " formatted as " + xmlNewFName
