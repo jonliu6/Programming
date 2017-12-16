aabd54aacef14c8bf698287c070d85121e1519de
Liu <jonliu6@gmail.com>
2017-12-16 00:24:33 -0800

    updated

---
+++ JavaDemo/src/org/freecode/demo/JavaVersionChecker.java
@@ -0,0 +1,8 @@
+package org.freecode.demo;
+
+public class JavaVersionChecker {
+
+	public static void main(String[] args) {
+		System.out.println("Java VM version: " + System.getProperty("sun.arch.data.model"));
+	}
+}
