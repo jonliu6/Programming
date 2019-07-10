a1137f11f03f85ff068f1c9111ff4c17f53f2b0b
Liu <Jonathan.Liu@bchydro.com>
2019-07-10 11:29:34 -0700

    updated jar package goals

--- SQLite3Demo/README.md
+++ SQLite3Demo/README.md
@@ -1,3 +1,7 @@
 This is an example of using SQLite-JDBC to access data.
 
 Knowledge_base.sqlite (DB file) - see the structure in sql\dbScripts.txt. When running, copy the DB file to the target folder - with SQLite3Demo.jar
+
+To build and package the application (into a .jar file), run "mvn clean package" in the project folder.
+
+To run the application, execute "java -jar SQLite3Demo.jar <the .sqlite3 database file name>"
--- SQLite3Demo/src/main/java/org/freecode/demo/SQLite3Tester.java
+++ SQLite3Demo/src/main/java/org/freecode/demo/SQLite3Tester.java
@@ -1,24 +1,55 @@
 package org.freecode.demo;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class SQLite3Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Properties props = new Properties();
+		String propertyFileName = "/app.properties";
+		String appName = null;
+		try {
+			props.load(SQLite3Tester.class.getResourceAsStream(propertyFileName));
+			appName = props.getProperty("application.name");
+		}
+		catch (Exception ex) {
+			System.err.println("Exception caught when loading the properties from " + propertyFileName + "\n" + ex.getMessage());
+			appName = "SQLite3Tester";
+		}
+		
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet res = null;
-        // String dbUrl = "jdbc:sqlite:/home/fedora/workspace/Programming/SQLite3Demo/src/main/resources/knowledge_base.sqlite3";
-		// String dbUrl = "jdbc:sqlite::resource:knowledge_base.sqlite";
-		String dbUrl = "jdbc:sqlite:knowledge_base.sqlite";
+        // String dbUrl = "jdbc:sqlite:/home/fedora/workspace/Programming/SQLite3Demo/src/main/resources/knowledge_base.sqlite3"; // absolute path in a Linux/Unix file system
+		// String dbUrl = "jdbc:sqlite::resource:knowledge_base.sqlite"; // since the sqlite3 database needs writable permission, cannot build into the .jar file
+		StringBuilder dbUrl = new StringBuilder("jdbc:sqlite:");
+		String dbFileName = null;
+		if (args == null || args.length < 1) {
+			dbFileName = appName + ".sqlite3";
+		}
+		else {
+			dbFileName = args[0];
+		}
+		
+		File dbFile = new File(dbFileName);
+		if (!dbFile.exists()) {
+			System.out.println("Error: cannot find a SQLite 3 database file - " + dbFileName + ".\nTry java -jar <.jar file name> <your database file full name>");
+			System.exit(0);
+		}
+		else {
+			dbUrl.append(dbFileName);
+		}
+		
+		// String dbUrl = "jdbc:sqlite:knowledge_base.sqlite"; // current folder (with the application)
         String testString = "Test Title";
         int num = 0;
         try {
-        	conn = DriverManager.getConnection(dbUrl);
+        	conn = DriverManager.getConnection(dbUrl.toString());
         	System.out.println("DB Connected");
         	stmt = conn.createStatement();
         	String sql = "INSERT INTO t_knowledge_base " +
