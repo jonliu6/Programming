7170632ea4bb3ac92938dd540acae3e632e951f4
Liu <Jonathan.Liu@bchydro.com>
2020-07-22 18:38:04 -0700

    added sample for FeatureLayer, Filter using ArcGIS JS APIs

---
+++ ArcGISJSAPIDemo/features.js
@@ -0,0 +1,72 @@
+var places = [
+  {
+    geometry: {
+      type: "point",
+      x: -123.1178758,
+      y: 49.2833356
+    },
+    attributes: {
+	  ObjectID: 1,
+	  "name": "CF Pacific Centre",
+	  "type": "Shopping Mall",
+      "address": "701 W Georgia St, Vancouver, BC V7Y 1G5",
+      "phone": "(604) 688-7235"
+    }
+  },
+  {
+    geometry: {
+      type: "point",
+      x: -123.1706035,
+      y: 49.1547209
+    },
+    attributes: {
+	  ObjectID: 2,
+	  "name": "Richmond Public Library",
+	  "type": "Library",
+      "address": "7700 Minoru Gate #100, Richmond, BC V6Y 1R8",
+      "phone": "(604) 231-6413"
+    }
+  },
+  {
+    geometry: {
+      type: "point",
+      x: -123.0836142,
+      y: 49.0382481
+    },
+    attributes: {
+	  ObjectID: 3,
+	  "name": "Tsawwassen Mills",
+	  "type": "Shopping Mall",
+      "address": "5000 Canoe Pass Way, Tsawwassen, BC V4M 0B3",
+      "phone": "(604) 948-9889"
+    }
+  },
+  {
+    geometry: {
+      type: "point",
+      x: -122.8319978,
+      y: 49.282651
+    },
+    attributes: {
+	  ObjectID: 4,
+	  "name": "Port Moody Public Library",
+	  "type": "Library",
+      "address": "100 Newport Dr, Port Moody, BC V3H 5C3",
+      "phone": "(604) 469-4575"
+    }
+  },
+  {
+    geometry: {
+      type: "point",
+      x: -123.0021739,
+      y: 49.2274657
+    },
+    attributes: {
+	  ObjectID: 5,
+	  "name": "Metropolis at Metrotown",
+	  "type": "Shopping Mall",
+      "address": "4700 Kingsway, Burnaby, BC V5H 4N2",
+      "phone": "(604) 438-4715"
+    }
+  }
+];
