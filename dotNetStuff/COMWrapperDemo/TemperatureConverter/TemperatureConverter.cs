7bed894aaa2982b4b768781444d2e0e863467d14
Liu <Jonathan.Liu@bchydro.com>
2020-09-16 17:22:35 -0700

    added COM Assembly Demo

---
+++ dotNetStuff/COMWrapperDemo/TemperatureConverter/TemperatureConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: AssemblyKeyFile("strongName.key.pfx")]
+namespace TemperatureConverter
+{
+    [ComVisible(true)]
+    [Guid("ca559bba-960b-44af-b637-0ec53b2fa4b9")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    public interface ITemperatureConverter
+    {
+        double GetCelsius(double fahrenheit);
+        double GetFahrenheit(double celsius);
+    }
+
+    [ComVisible(true)]
+    [Guid("8b327f11-70a7-47f0-a20d-a524a26847b1")]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class TemperatureConverter: ITemperatureConverter
+    {
+        public double GetCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+
+        public double GetFahrenheit(double celsius)
+        {
+            return celsius * 9 / 5 + 32;
+        }
+    }
+}
---
+++ dotNetStuff/COMWrapperDemo/TemperatureConverter/readme.txt
@@ -0,0 +1,15 @@
+﻿This COM library is built by .Net Framework 4.6.2.
+
+To create and sign a strong name assembly in Visual Studio, 
+  1. right-click the project and choose Proerpties
+  2. navigate to Signing tab and enable Sign the Assembly option
+  3. click the dropdown below "Choose a strong name key file" and choose New...
+  4. fill in name, password and etc and click OK.
+  5. add [assembly: AssemblyKeyFile("keyfilename")] at the top of the class file
+  6. rebuild the solution
+
+To register, open a Command Window as Administrator and run:
+  c:\Windows\Microsoft.NET\Framework\v<version>\RegAsm.exe TemperatureConverter.dll /codebase /tlb:TemperatureConverter.tlb
+
+To un-register, run:
+  c:\Windows\Microsoft.NET\Framework\v<version>\RegAsm.exe TemperatureConverter.dll /unregister
