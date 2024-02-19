389ebf1f56dd2e7e8d4f3092152278adc30adf1c
J L <jonliu6@gmail.com>
2024-02-18 23:43:33 -0800

    added system time to the registration page

--- SpringBootWeb/src/main/java/org/freecode/demo/controller/RegistrationController.java
+++ SpringBootWeb/src/main/java/org/freecode/demo/controller/RegistrationController.java
@@ -1,5 +1,12 @@
 package org.freecode.demo.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.freecode.demo.model.RegistrationFormObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,10 +14,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.validation.Valid;
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 /**
@@ -24,6 +27,7 @@ public class RegistrationController {
     public String registerForm(Model model) {
         model.addAttribute("registrationDetails", new RegistrationFormObject());
         model.addAttribute("professions", professionList);
+        model.addAttribute("currentTime", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime()));
 
         return "registrationForm";
     }
