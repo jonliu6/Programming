13836bb925eab5d5fbd8d445787a4eb6b891819f
J L <jonliu6@gmail.com>
2024-03-20 22:49:13 -0700

    fixed security alerts

--- SpringSecurity/src/main/java/org/freecode/demo/App.java
+++ SpringSecurity/src/main/java/org/freecode/demo/App.java
@@ -1,6 +1,7 @@
 package org.freecode.demo;
 
-import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+//import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.core.token.Sha512DigestUtils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -13,18 +14,18 @@ public class App
     public static void main( String[] args )
     {
         // System.out.println( "Hello World!" );
-    	Md5PasswordEncoder encoder = new Md5PasswordEncoder();
+    	// Md5PasswordEncoder encoder = new Md5PasswordEncoder();
     	BCryptPasswordEncoder encoder1 = new BCryptPasswordEncoder();
     	String userName = "jon";
-    	System.out.println("MD5: " + encoder.encodePassword(userName, null));
+    	//System.out.println("MD5: " + encoder.encodePassword(userName, null));
     	System.out.println("BCrypt: " + encoder1.encode(userName));
         System.out.println("SHA: " + Sha512DigestUtils.shaHex(userName)); 
         userName = "po";
-    	System.out.println("MD5: " + encoder.encodePassword(userName, null));
+    	//System.out.println("MD5: " + encoder.encodePassword(userName, null));
     	System.out.println("BCrypt: " + encoder1.encode(userName));
         System.out.println("SHA: " + Sha512DigestUtils.shaHex(userName));
         userName = "user";
-    	System.out.println("MD5: " + encoder.encodePassword(userName, null));
+    	//System.out.println("MD5: " + encoder.encodePassword(userName, null));
     	System.out.println("BCrypt: " + encoder1.encode(userName));
         System.out.println("SHA: " + Sha512DigestUtils.shaHex(userName)); 
     }
--- SpringSecurity/src/main/java/org/freecode/demo/controller/LoginController.java
+++ SpringSecurity/src/main/java/org/freecode/demo/controller/LoginController.java
@@ -1,8 +1,5 @@
 package org.freecode.demo.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -12,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
