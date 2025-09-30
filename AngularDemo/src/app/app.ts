8b7af5d9fd2a74a28b8d6b91bbf53357c7e2f2e4
J Liu <jonliu6@gmail.com>
2025-09-30 11:55:31 -0700

    Angular initial demo

---
+++ AngularDemo/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  apptitle = 'Angular Demo';
+  protected readonly title = signal(this.apptitle);
+}
---
+++ AngularDemo/src/app/content.ts
@@ -0,0 +1,8 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.html',
+  styleUrl: './app.css'
+})
+export class Content { }
